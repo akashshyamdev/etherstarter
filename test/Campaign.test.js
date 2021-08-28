910e31d708c814e4abb6510a5410f276852575b6
Akash Shyam <shyamiy@gmail.com>
2021-08-28 15:32:43 +0530

    🚧  compiler

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,30 +1,37 @@
-import assert from 'assert';
-import ganache from 'ganache-cli';
-import Web3 from 'web3';
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-import compiledFactory from '../backend/build/CampaignFactory.json';
-import compiledCampaign from '../backend/build/Campaign.json';
+const compiledFactory = require('../backend/build/CampaignFactory.json');
+const compiledCampaign = require('../backend/build/Campaign.json');
 
-let accounts: string[];
-let factory: any;
-let campaignAddress: string;
-let campaign: any;
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
 
 beforeEach(async () => {
-	console.log(JSON.parse(compiledFactory).evm.abi);
-	console.log(JSON.parse(compiledCampaign).evm.abi);
+	// console.log(JSON.parse(compiledFactory).abi);
+	// console.log(JSON.parse(compiledCampaign));
 
 	accounts = await web3.eth.getAccounts();
 
-	factory = await new web3.eth.Contract(JSON.parse(compiledFactory).evm.abi)
-		.deploy(JSON.parse(compiledFactory).bytecode)
+	factory = await new web3.eth.Contract(JSON.parse(compiledFactory).abi)
+		.deploy(`0x${JSON.parse(compiledFactory).evm.bytecode.object}`)
 		.send({ from: accounts[0], gas: 1000000 });
 
 	await factory.methods.createCampaign('100').send({ from: accounts[0], gas: 1000000 });
 
 	[campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
-	campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign).evm.abi, campaignAddress);
+	campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign).abi, campaignAddress);
+});
+
+describe('campaign', () => {
+	it('deploys a factory and a campaign', () => {
+		assert.ok(factory.options.address);
+		assert.ok(campaign.options.address);
+	});
 });
