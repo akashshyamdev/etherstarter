70fff561ae207bbd9a29009c83f2ef77944648f2
Akash Shyam <shyamiy@gmail.com>
2021-08-28 23:06:56 +0530

    ♻️ refactor: updated infura link for web3

--- src/services/web3.ts
+++ src/services/web3.ts
@@ -8,7 +8,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
 	web3 = new Web3(window.ethereum);
 } else {
 	// We are on the server *OR* the user is not running metamask
-	const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c');
+	const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/21e523b9f70746289fb86611a47d199e');
 	web3 = new Web3(provider);
 }
 
