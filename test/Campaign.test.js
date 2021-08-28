7f6343b37c84018efdbb0d9581fdf513262bf910
Akash Shyam <shyamiy@gmail.com>
2021-08-28 16:32:44 +0530

    ✅ added tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -4,8 +4,22 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require('../backend/build/CampaignFactory.json');
-const compiledCampaign = require('../backend/build/Campaign.json');
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+const cConfig = {
+	cColors: {
+		green: '\x1b[36m%s\x1b[32m',
+		yellow: '\x1b[36m%s\x1b[33m',
+		red: '\x1b[36m%s\x1b[31m',
+	},
+	cUnicodes: {
+		check: '✓',
+		cross: '⨯',
+		ether: '⧫',
+	},
+	cSpaces: (ns) => Array(ns + 1).join(' '),
+};
 
 let accounts;
 let factory;
@@ -15,20 +29,34 @@ let campaign;
 beforeEach(async () => {
 	accounts = await web3.eth.getAccounts();
 
-	factory = await new web3.eth.Contract(JSON.parse(compiledFactory).abi)
-		.deploy(`0x${JSON.parse(compiledFactory).evm.bytecode.object}`)
-		.send({ from: accounts[0], gas: 1000000 });
+	const contract = await new web3.eth.Contract(compiledFactory.abi);
+	const deploy = await contract.deploy({ data: '0x' + compiledFactory.evm.bytecode.object });
 
-	await factory.methods.createCampaign('100').send({ from: accounts[0], gas: 1000000 });
+	factory = await deploy.send({ from: accounts[0], gas: '1500000' });
 
-	[campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+	await factory.methods.createCampaign('100').send({ from: accounts[0], gas: '1500000' });
 
-	campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign).abi, campaignAddress);
+	[campaignAddress] = await factory.methods.getCampaigns().call();
+	campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
 });
 
-describe('campaign', () => {
+describe('Campaigns', () => {
 	it('deploys a factory and a campaign', () => {
 		assert.ok(factory.options.address);
 		assert.ok(campaign.options.address);
+	});
+
+	it('caller as campaign manager', async () => {
+		const manager = await campaign.methods.manager().call();
+
+		assert.strictEqual(accounts[0], manager);
+	});
+
+	it('contribute money as approvers', async () => {
+		await campaign.methods.contribute().send({ from: accounts[1], value: '200' });
+
+		const isContributor = await campaign.methods.approvers(accounts[1]).call();
+
+		assert(isContributor);
 	});
 });
