6301c0c383b89f14ce23ac62e4285d4fc107ca5c
Akash Shyam <shyamiy@gmail.com>
2021-08-29 15:17:26 +0530

    💄 style(create campaign): form

---
+++ src/pages/campaigns/new.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Heading from '../../components/Heading';
+
+export default function NewCampaign() {
+	const [amount, setAmount] = React.useState<number | null>(null);
+
+	return (
+		<div>
+			<Heading>New Campaign</Heading>
+
+			<div className='w-full max-w-xs'>
+				<form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+					<div className='mb-4'>
+						<label
+							className='block font-display tracking-wide text-gray-700 text-sm font-bold mb-2'
+							htmlFor='username'
+						>
+							Minimum Contribution(wei)
+						</label>
+
+						<input
+							id='username'
+							type='number'
+							value={amount}
+							placeholder='1000'
+							onChange={(e) => setAmount(parseInt(e.target.value))}
+							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+						/>
+					</div>
+
+					<div className='flex items-center justify-between mt-10'>
+						<button
+							className='bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+							type='button'
+						>
+							Create Campaign
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
