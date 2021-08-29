d01e331673949bb68b1b5c0a6141617882764207
Akash Shyam <shyamiy@gmail.com>
2021-08-29 15:58:45 +0530

    💄 style(alert) improved spacing

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -13,7 +13,15 @@ export default function Alert({ children, className }: AlertProps) {
 				<XCircleIcon className='w-5 h-5 mr-2' /> There was a problem
 			</h6>
 
-			<ul>{typeof children === 'string' ? <p>{children}</p> : children.map((child) => <li>{child}</li>)}</ul>
+			{typeof children === 'string' ? (
+				<p>{children}</p>
+			) : (
+				children.map((child) => (
+					<ul>
+						<li>{child}</li>
+					</ul>
+				))
+			)}
 		</div>
 	);
 }
