58e6e36e48bc0bfa3d7001d90ce5233faf73add5
Akash Shyam <shyamiy@gmail.com>
2021-08-29 12:08:49 +0530

    ✨ feat: campaign list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Link from 'next/link';
 import factory from '../services/factory';
 
 export async function getServerSideProps(context) {
@@ -17,11 +18,23 @@ export default function CampaignList({ campaigns }) {
 	});
 
 	return (
-		<div>
-			<h1 className='text-red-500'>Campaign List</h1>
-			{campaigns.map((address: string) => (
-				<h4 className='text-red-500'>{address}</h4>
-			))}
-		</div>
+		<main className='w-full text-gray-700 py-10 px-16 font-sans'>
+			<div className='flex flex-row justify-between'>
+				<h1 className='text-6xl mb-10 font-display'>Campaign List</h1>
+				<button className='bg-blue-700 text-white rounded-md h-24 text-2xl px-10 py-3'>Create Campaign</button>
+			</div>
+
+			<div className='w-3/5'>
+				{campaigns.map((address: string) => (
+					<div key={address} className='border rounded-lg px-7 py-7'>
+						<h4 className='text-2xl'>{address}</h4>
+
+						<Link href='/'>
+							<a className='text-blue-600 mt-5'>View Campaign</a>
+						</Link>
+					</div>
+				))}
+			</div>
+		</main>
 	);
 }
