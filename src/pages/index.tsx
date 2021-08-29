b2d21e639b3c656fa99e500bbf9d394acc93dd5a
Akash Shyam <shyamiy@gmail.com>
2021-08-29 09:52:18 +0530

    ✨ feat: fetched campaigns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from 'react';
 import factory from '../services/factory';
 
-export default function CampaignList() {
+export async function getServerSideProps(context) {
+	const campaigns = await factory.methods?.getCampaigns().call();
+
+	return {
+		props: { campaigns },
+	};
+}
+
+export default function CampaignList({ campaigns }) {
 	useEffect(() => {
 		(async () => {
-			const campaign = await factory.methods.getDeployedCampaigns().call();
+			console.log(campaigns);
 		})();
 	});
 
 	return (
 		<div>
-			<h1>Campaign List</h1>
+			<h1 className='text-red-500'>Campaign List</h1>
+			{campaigns.map((address: string) => (
+				<h4 className='text-red-500'>{address}</h4>
+			))}
 		</div>
 	);
 }
