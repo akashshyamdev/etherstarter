d43997567b2ae4b5c0633e8b72c3d4ba95e986d9
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-29 20:20:49 +0530

    ✨ feat: form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,55 @@
+import React, { FormEventHandler, InputHTMLAttributes } from 'react';
+import Loader from './Loader';
+
+interface FormData {
+	name: string;
+	label: string;
+	placeholder: string;
+	inputType?: InputHTMLAttributes<HTMLInputElement>['type'];
+}
+
+interface FormProps {
+	formState: Object;
+	data: FormData[];
+	setFormState: React.Dispatch<React.SetStateAction<FormProps['formState']>>;
+	onSubmit: FormEventHandler<HTMLFormElement>;
+}
+
+export default function Form({ onSubmit, data, formState, setFormState }: FormProps) {
+	const [loading, setLoading] = React.useState(false);
+
+	return (
+		<form onSubmit={onSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+			{data.map(({ label, placeholder, name, inputType }) => (
+				<div className='mb-4'>
+					<label className='block font-display tracking-wide text-gray-700 text-sm font-bold mb-2' htmlFor={name}>
+						{label}
+					</label>
+
+					<input
+						required
+						id={name}
+						type={inputType || 'text'}
+						value={formState[name]}
+						placeholder={placeholder}
+						onChange={(e) => setFormState({ ...formState, [name]: e.target.value })}
+						className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+					/>
+				</div>
+			))}
+
+			<div className='flex items-center justify-between mt-10'>
+				{!loading ? (
+					<button
+						type='submit'
+						className='bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+					>
+						Create Campaign
+					</button>
+				) : (
+					<Loader />
+				)}
+			</div>
+		</form>
+	);
+}
