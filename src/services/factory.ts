7c0ac75f10caa864c5b0ce046c6f0f62c56ff91f
Akash Shyam <shyamiy@gmail.com>
2021-08-29 09:50:08 +0530

    🐛 fix(factory): bug with abi

--- src/services/factory.ts
+++ src/services/factory.ts
@@ -2,6 +2,6 @@ import web3 from './web3';
 import CampaignFactory from '../../ethereum/build/CampaignFactory.json';
 
 // @ts-ignore
-const instance = new web3.eth.Contract(JSON.parse(CampaignFactory).abi, '0x9C0f2B23Cb7EC700605433B2aFa9EDd0b6881390');
+const instance = new web3.eth.Contract(CampaignFactory.abi, '0x9C0f2B23Cb7EC700605433B2aFa9EDd0b6881390');
 
 export default instance;
