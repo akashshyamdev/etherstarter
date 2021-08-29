c654c0bd9ee8ecdf7adf04a733ff67beeb869377
Akash Shyam <shyamiy@gmail.com>
2021-08-29 15:20:50 +0530

    ✨ feat(create campaign): form submission logic

--- src/pages/campaigns/new.tsx
+++ src/pages/campaigns/new.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import web3 from '../../services/web3';
+import factory from '../../services/factory';
 import Heading from '../../components/Heading';
 
 export default function NewCampaign() {
 	const [amount, setAmount] = React.useState<number | null>(null);
+
+	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const accounts = web3.eth.getAccounts();
+
+		await factory.methods.createCampaign(amount).send({ from: accounts[0] });
+	};
 
 	return (
 		<div>
 			<Heading>New Campaign</Heading>
 
 			<div className='w-full max-w-xs'>
-				<form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+				<form onSubmit={onSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
 					<div className='mb-4'>
 						<label
 							className='block font-display tracking-wide text-gray-700 text-sm font-bold mb-2'
