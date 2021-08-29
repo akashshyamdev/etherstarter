47ed099ea780102fe9711329aa8cfb3c1fc64351
Akash Shyam <shyamiy@gmail.com>
2021-08-29 12:08:03 +0530

    🔧 chore(tailwind): added fonts to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		fontFamily: {
+			sans: ['Roboto', 'system-ui'],
+			serif: ['ui-serif', 'Georgia'],
+			mono: ['ui-monospace', 'SFMono-Regular'],
+			display: ['Poppins'],
+			body: ['Open Sans'],
+		},
 		extend: {},
 	},
 	variants: {
