6c35cb1a0064895aab489cd0cc9dc933446d73f4
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-29 20:36:34 +0530

    ♻️ refactor(new campaign): used form component

--- src/pages/campaigns/new.tsx
+++ src/pages/campaigns/new.tsx
@@ -5,11 +5,12 @@ import Heading from '../../components/Heading';
 import Alert from '../../components/Alert';
 import { useRouter } from 'next/dist/client/router';
 import Loader from '../../components/Loader';
+import Form from '../../components/Form';
 
 export default function NewCampaign() {
 	const router = useRouter();
 
-	const [amount, setAmount] = React.useState<number | null>(0);
+	const [formData, setFormData] = React.useState({ minContribution: 0 });
 	const [loading, setLoading] = React.useState<boolean>(false);
 	const [message, setMessage] = React.useState<string>('');
 
@@ -22,7 +23,7 @@ export default function NewCampaign() {
 		try {
 			const accounts = await web3.eth.getAccounts();
 
-			await factory.methods.createCampaign(amount).send({ from: accounts[0] });
+			await factory.methods.createCampaign(formData.minContribution).send({ from: accounts[0] });
 
 			router.push('/');
 		} catch (err) {
@@ -38,39 +39,14 @@ export default function NewCampaign() {
 			{message && <Alert>{message}</Alert>}
 
 			<div className='w-full max-w-xs mt-10'>
-				<form onSubmit={onSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-					<div className='mb-4'>
-						<label
-							className='block font-display tracking-wide text-gray-700 text-sm font-bold mb-2'
-							htmlFor='username'
-						>
-							Minimum Contribution(wei)
-						</label>
-
-						<input
-							required
-							id='username'
-							type='number'
-							value={amount}
-							placeholder='1000'
-							onChange={(e) => setAmount(parseInt(e.target.value))}
-							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-						/>
-					</div>
-
-					<div className='flex items-center justify-between mt-10'>
-						{!loading ? (
-							<button
-								type='submit'
-								className='bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-							>
-								Create Campaign
-							</button>
-						) : (
-							<Loader />
-						)}
-					</div>
-				</form>
+				<Form
+					onSubmit={onSubmit}
+					data={[
+						{ name: 'minContribution', label: 'Minimum Contribution(wei)', placeholder: '1000', inputType: 'number' },
+					]}
+					formState={formData}
+					setFormState={setFormData}
+				/>
 			</div>
 		</div>
 	);
