aaef8a86e2c2a6ad7395c42799ae8ed811ce6e2d
Akash Shyam <shyamiy@gmail.com>
2021-08-29 15:59:11 +0530

    🐛 fix: conditionally rendered alerts

--- src/pages/campaigns/new.tsx
+++ src/pages/campaigns/new.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+
 import web3 from '../../services/web3';
 import factory from '../../services/factory';
 import Heading from '../../components/Heading';
+import Alert from '../../components/Alert';
 
 export default function NewCampaign() {
-	const [amount, setAmount] = React.useState<number | null>(null);
+	const [amount, setAmount] = React.useState<number | null>(0);
+	const [message, setMessage] = React.useState<string>('');
 
 	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
-		const accounts = web3.eth.getAccounts();
+		try {
+			const accounts = await web3.eth.getAccounts();
 
-		await factory.methods.createCampaign(amount).send({ from: accounts[0] });
+			await factory.methods.createCampaign(amount).send({ from: accounts[0] });
+		} catch (err) {
+			setMessage(err.message);
+		}
 	};
 
 	return (
 		<div>
 			<Heading>New Campaign</Heading>
+
+			{message && <Alert>{message}</Alert>}
 
 			<div className='w-full max-w-xs'>
 				<form onSubmit={onSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
@@ -41,8 +50,8 @@ export default function NewCampaign() {
 
 					<div className='flex items-center justify-between mt-10'>
 						<button
+							type='submit'
 							className='bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-							type='button'
 						>
 							Create Campaign
 						</button>
