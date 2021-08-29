827520391cec95900f71a7b44b599aee166c0d66
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-29 19:58:55 +0530

    💄 style(card): added truncate

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,15 @@ import { connect } from 'http2';
 import React from 'react';
 
 interface CardProps {
-	title: string;
+	title: string | number;
 	subtitle: string;
 	content: string;
 }
 
 export default function Card({ content, subtitle, title }: CardProps) {
 	return (
-		<div className='border-2 py-5 px-5'>
-			<h3 className='text-3xl'>{title}</h3>
+		<div className='border-2 py-5 px-5 rounded w-3/5'>
+			<h3 className='text-3xl truncate'>{title}</h3>
 			<h6 className='text-lg text-gray-400'>{subtitle}</h6>
 
 			<p>{content}</p>
