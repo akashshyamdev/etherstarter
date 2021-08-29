ca4a035f6a16d3e55fe37ee72f0c69a7bb20744b
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-29 18:26:34 +0530

    ✨ feat(heading): added support for custom clasname

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function Heading({ children }) {
-	return <h1 className='text-6xl mb-10 font-display'>{children}</h1>;
+export interface HeadingProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+export default function Heading({ children, className }: HeadingProps) {
+	return <h1 className={`text-6xl mb-10 font-display ${className}`}>{children}</h1>;
 }
