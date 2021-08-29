86497ccba25a3bb1f4ca5e5ae1ed5ec4a9ad0e83
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-29 18:38:22 +0530

    ♻️ refactor(factory): updated address

--- src/services/factory.ts
+++ src/services/factory.ts
@@ -2,6 +2,6 @@ import web3 from './web3';
 import CampaignFactory from '../../ethereum/build/CampaignFactory.json';
 
 // @ts-ignore
-const instance = new web3.eth.Contract(CampaignFactory.abi, '0x9C0f2B23Cb7EC700605433B2aFa9EDd0b6881390');
+const instance = new web3.eth.Contract(CampaignFactory.abi, '0xc85F2F9F07aa063326a089F4d1288D45b86a77f2');
 
 export default instance;
