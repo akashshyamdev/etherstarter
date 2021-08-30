dbd871764d99cefbeed276be46566dc16eeb06fc
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-30 10:43:24 +0530

    ✨ feat(new request): added functionality to form

--- src/pages/campaigns/[address]/requests/new.tsx
+++ src/pages/campaigns/[address]/requests/new.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import { useRouter } from 'next/dist/client/router';
+import { FormEvent, useState } from 'react';
+import web3 from '../../../../services/web3';
 import Form from '../../../../components/Form';
 import Heading from '../../../../components/Heading';
+import createCampaignInstance from '../../../../services/campaign';
+import Alert from '../../../../components/Alert';
 
 export default function NewRequest() {
-	const [formData, setFormData] = React.useState({ minContribution: 0 });
-	const [loading, setLoading] = React.useState<boolean>(false);
-	const [message, setMessage] = React.useState<string>('');
+	const router = useRouter();
+	const { address } = router.query;
 
-	const onSubmit = () => {};
+	const [formData, setFormData] = useState({ description: '', amount: 0, receiver: '' });
+	const [loading, setLoading] = useState<boolean>(false);
+	const [message, setMessage] = useState<string>('');
+
+	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		setLoading(true);
+		setMessage('');
+
+		const campaign = createCampaignInstance(address as string);
+
+		try {
+			const accounts = await web3.eth.getAccounts();
+
+			await campaign.methods
+				.createRequest(formData.description, web3.utils.toWei(formData.amount, 'ether'), formData.receiver)
+				.send({ from: accounts[0] });
+
+			setFormData({ description: '', amount: 0, receiver: '' });
+			setLoading(false);
+		} catch (err) {
+			setMessage(err.message);
+			setLoading(false);
+			setFormData({ description: '', amount: 0, receiver: '' });
+		}
+	};
 
 	return (
 		<div>
 			<Heading>Create Request</Heading>
+			{message && <Alert>{message}</Alert>}
 
 			<Form
 				loading={loading}
