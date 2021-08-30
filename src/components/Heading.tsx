81b6cc657d927358fae3920016dafe5962cd0ad2
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-30 10:16:22 +0530

    ✨ feat(heading): added customizable style property

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 export interface HeadingProps {
-	children: React.ReactNode;
 	className?: string;
+	children: React.ReactNode;
+	style?: HTMLAttributes<HTMLHeadingElement>['style'];
 }
 
-export default function Heading({ children, className }: HeadingProps) {
-	return <h1 className={`text-6xl mb-10 font-display ${className}`}>{children}</h1>;
+export default function Heading({ style, children, className }: HeadingProps) {
+	return (
+		<h1 style={style} className={`text-6xl mb-10 font-display ${className}`}>
+			{children}
+		</h1>
+	);
 }
