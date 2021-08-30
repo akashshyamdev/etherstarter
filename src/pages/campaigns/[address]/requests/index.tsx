c84714b61c9c637aabc5fbf920e0cd56ab9a726a
Akash Shyam <akash.shyam2008@gmail.com>
2021-08-30 10:06:24 +0530

    ✨ feat(requests): create request link

--- src/pages/campaigns/[address]/requests/index.tsx
+++ src/pages/campaigns/[address]/requests/index.tsx
@@ -1,10 +1,19 @@
+import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
 import React from 'react';
 import Heading from '../../../../components/Heading';
 
 export default function Requests() {
+	const router = useRouter();
+	const { address } = router.query;
+
 	return (
 		<div>
 			<Heading>Request List</Heading>
+
+			<Link href={`/campaigns/${address}/requests/new`}>
+				<a className='text-blue-600 mt-5'>View Campaign</a>
+			</Link>
 		</div>
 	);
 }
